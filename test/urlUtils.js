35f7bfc558043ebebe742d438d239c804938d172
Kondrat Shmoylov <kondrat@yandex-team.ru>
2014-07-14 18:59:24 +0400

    fix of test

--- test/urlUtils.js
+++ test/urlUtils.js
@@ -1,60 +1,65 @@
-/* global QUnit */
+/* global x, equal, ok, QUnit */
+/* jshint multistr: true */
+(function() {
+    "use strict";
 
-QUnit.module('franky');
+    QUnit.module('franky');
 
-test("test for x.constructURL", function () {
+    test("test for x.constructURL", function () {
 
-    equal(
-        x.constructURL("http://www.yandex.ru", {
-            "text": 1,
-            "hello": "world"
-        }),
-        "http://www.yandex.ru?text=1&hello=world",
-        "OK"
-    );
+        equal(
+            x.constructURL("http://www.yandex.ru", {
+                "text": 1,
+                "hello": "world"
+            }),
+            "http://www.yandex.ru?text=1&hello=world",
+            "OK"
+        );
 
-    equal(
-        x.constructURL("http://www.google.com?", {
-            "search": "ololo",
-            "test": "ok"
-        }),
-        "http://www.google.com?search=ololo&test=ok",
-        "OK"
-    );
+        equal(
+            x.constructURL("http://www.google.com?", {
+                "search": "ololo",
+                "test": "ok"
+            }),
+            "http://www.google.com?search=ololo&test=ok",
+            "OK"
+        );
 
-    equal(
-        x.constructURL("http://www.google.com?", {
-            "text": ""
-        }),
-        "http://www.google.com?text=",
-        "OK"
-    );
+        equal(
+            x.constructURL("http://www.google.com?", {
+                "text": ""
+            }),
+            "http://www.google.com?text=",
+            "OK"
+        );
 
-    equal(
-        x.constructURL("http://www.yandex.com/test/?vow=test", {
-            "search": "ololo",
-            "test": "ok"
-        }),
-        "http://www.yandex.com/test/?vow=test&search=ololo&test=ok",
-        "OK"
-    );
+        equal(
+            x.constructURL("http://www.yandex.com/test/?vow=test", {
+                "search": "ololo",
+                "test": "ok"
+            }),
+            "http://www.yandex.com/test/?vow=test&search=ololo&test=ok",
+            "OK"
+        );
 
-    equal(
-        x.constructURL("http://www.yandex.com:5674/test/?vow=test", {
-            "search": "ololo",
-            "test": "ok"
-        }),
-        "http://www.yandex.com:5674/test/?vow=test&search=ololo&test=ok",
-        "OK"
-    );
+        equal(
+            x.constructURL("http://www.yandex.com:5674/test/?vow=test", {
+                "search": "ololo",
+                "test": "ok"
+            }),
+            "http://www.yandex.com:5674/test/?vow=test&search=ololo&test=ok",
+            "OK"
+        );
 
-    equal(
-        x.constructURL("https://www.mail.ru?a=1#anchor", {
-            "areurobot": "yes",
-            "text": "blah"
-        }),
-        "https://www.mail.ru?a=1&areurobot=yes&text=blah#anchor",
-        "OK"
-    );
+        equal(
+            x.constructURL("https://www.mail.ru?a=1#anchor", {
+                "areurobot": "yes",
+                "text": "blah"
+            }),
+            "https://www.mail.ru?a=1&areurobot=yes&text=blah#anchor",
+            "OK"
+        );
 
-});
+    });
+
+})();
