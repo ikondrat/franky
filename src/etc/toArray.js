e9952b963f1e1c1e9320077c5db9e342558ece2c
Kondrat Shmoylov <kondrat@yandex-team.ru>
2014-12-08 19:21:49 +0300

    changes in array convert method

--- src/etc/toArray.js
+++ src/etc/toArray.js
@@ -3,11 +3,7 @@ define([
     '../checks/isArrayLike'
 ], function( franky ) {
     // Converts object-like structures to array
-    franky.toArray = function (/**Object*/obj) /**Array*/ {
-        var res = [];
-        if (obj && franky.isArrayLike(obj)) {
-            return Array.prototype.slice.call(obj);
-        }
-        return res;
+    franky.toArray = function (/**Object*/smth) /**Array*/ {
+        return Array.prototype.slice.call(smth);
     };
 });
