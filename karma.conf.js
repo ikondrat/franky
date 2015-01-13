d303d15b44d1127aec935e2a2c9d787a01351fd1
Kondrat Shmoylov <kondrat@yandex-team.ru>
2015-01-13 12:40:13 +0300

    correct location of builded file

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'dist/franky.js',
+      'lib/franky.js',
       'test/*.js'
     ],
 
