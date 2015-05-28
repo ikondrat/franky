609f17015ccb95e4a15b2d3e24c0fd52fef223b5
Kondrat Shmoylov <kondrat@yandex-team.ru>
2015-05-28 15:13:38 +0300

    fix uncorrect variable

--- src/etc/decamelCase.js
+++ src/etc/decamelCase.js
@@ -1,11 +1,11 @@
 define([
     './../core',
 ], function (franky) {
-    var camelCases = /([a-z]{1})([A-Z]{1})/g;
+    var dashedCases = /([a-z]{1})([A-Z]{1})/g;
     // Converts camelSyntax syntax to dashedSyntax
     // >x.decamelCase('helloWorld') -> 'hello-world'
     franky.decamelCase = function (/**String*/str) /**String*/ {
-        return str.replace(camelCases, function(all, letterFirst, letterSecond) {
+        return str.replace(dashedCases, function(all, letterFirst, letterSecond) {
             return letterFirst + "-" + letterSecond.toLowerCase();
         });
     };
